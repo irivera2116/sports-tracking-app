9e7c90c57a9ba4b73e3974de798d1a424a7e262f
Justin <jhud1998@gmail.com>
2023-07-11 20:28:31 -0500

    Add all connections to Server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+
+const StoreSequelize = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//Possibly Needs session store varible *cookies* etc etc
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
