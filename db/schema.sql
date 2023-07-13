3a327a8232b65a534524a12992231fe2863ca181
hkchebli <38819813+hkchebli@users.noreply.github.com>
2023-07-13 19:08:56 -0400

    done with schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,38 @@
+DROP DATABASE IF EXISTS chat_db
+
+CREATE DATABASE chat_db;
+
+USE chat_db;
+
+CREATE TABLE login_info (
+  id INTEGER AUTO_INCREMENT NOT NULL,
+  user_name VARCHAR(255) NOT NULL,
+  user_password VARCHAR(255) NOT NULL,
+  isAdmin BOOLEAN DEFAULT false,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE users (
+  id INTEGER AUTO_INCREMENT NOT NULL,
+  login_id INTEGER NOT NULL,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  display_name VARCHAR(30),
+  --avatar_dirct VARCHAR(30),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE messages (
+  id INTEGER AUTO_INCREMENT NOT NULL,
+  user_id INTEGER NOT NULL,
+  room_id INTEGER NOT NULL,
+  message_body VARCHAR(300),
+  PRIMARY KEY(id)
+);
+
+
+CREATE TABLE rooms (
+  id INTEGER AUTO_INCREMENT NOT NULL,
+  room_name VARCHAR(50),
+  PRIMARY KEY(id)
+);
