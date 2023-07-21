9cdcbef2270918434c6a4517ec097efcefe9918d
Nigel D. Watkins <nigeldwatkins@gmail.com>
2023-07-21 18:09:29 -0400

    working branch

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const { Project, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,24 +4,23 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const sequelize = require('./config/connection');
 const StoreSequelize = require('connect-session-sequelize')(session.Store);
-
+const scoresRouter = require('./controllers/api/scores');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-
+const axios = require('axios');
+const { rapidAPIKey } = require('./config/key')
 const PORT = process.env.PORT || 3001;
-
 const routes = require('./controllers');
-app.use('/', routes);
-/*const myMiddleware = (req, res, next) => {
-  console.log('This is a middleware function.');
-  next();
-};*/
-//app.use(myMiddleware);
+
+
+app.use(routes);
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs({defaultLayout: 'layouts'}));
 app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
 
 app.engine('handlebars', exphbs());
 //app.set('view engine', 'handlebars');
@@ -29,6 +28,39 @@ app.engine('handlebars', exphbs());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api/scores', scoresRouter);
+
+app.get('/', async (req, res) => {
+  try {
+    const options = {
+      method: 'GET',
+      url: 'https://allscores.p.rapidapi.com/api/allscores/games-scores',
+      params: {
+        startDate: '18/01/20223',
+        langId: '1',
+        sport: '1',
+        endDate: '18/01/20223',
+        timezone: 'America/Chicago',
+        onlyMajorGames: 'true',
+        withTop: 'true'
+      },
+      headers: {
+        'X-RapidAPI-Key': rapidAPIKey,
+        'X-RapidAPI-Host': 'allscores.p.rapidapi.com'
+      }
+    };
+
+    const response = await axios.request(options);
+    const scoresData = response.data;
+
+    console.log(scoresData); // logs the data
+
+    res.render('homepage', { scoresData: scoresData || [] }); // Pass empty array if scoresData is undefined
+  } catch (error) {
+    console.error('Error fetching scores:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 // Create a session store and use it
 const sessionStore = new StoreSequelize({ db: sequelize });
